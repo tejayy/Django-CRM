c0739a5721bd9c0b865d0efd386b8e4143d1bd07
star <tbj456@gmail.com>
2023-05-03 16:09:53 +0530

    Final Project

--- website/urls.py
+++ website/urls.py
@@ -3,7 +3,6 @@ from website import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-   #path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('record/<int:pk>/', views.customer_record, name='record'),
